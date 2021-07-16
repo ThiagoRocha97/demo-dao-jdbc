5bc210413229589c299fba83629c438aeb64b6dd
ThiagoRocha97 <timmyrocha97@yahoo.com>
2021-07-16 11:10:19 -0300

    SellerDaoJDBC and DaoFactory

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -5,7 +5,7 @@ import model.entities.Seller;
 import java.util.List;
 
 public interface SellerDao {
-    void insert(Seller obj); 
+    void insert(Seller obj);
     void update(Seller obj);
     void deleteById(Integer id);
     Seller findById(Integer id);
