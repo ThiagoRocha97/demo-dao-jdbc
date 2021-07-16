c496930f6e9fa6578269e71b111682ad4243421c
ThiagoRocha97 <timmyrocha97@yahoo.com>
2021-07-16 11:56:52 -0300

    findById implementation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -104,7 +104,7 @@ public class Seller implements Serializable {
 
     @Override
     public String toString() {
-        return "Seller{" +
+        return "Seller {" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", email='" + email + '\'' +
